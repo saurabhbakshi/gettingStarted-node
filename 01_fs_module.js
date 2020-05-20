ad761bffa2d1d13ce7919a7f0125d318cf3faab3
Saurabh Jain <sj652744@broadcom.net>
2020-05-20 16:44:53 +0530

    Initial commit example fs module

---
+++ 01_fs_module.js
@@ -0,0 +1,9 @@
+// Node module to work with Files
+
+// Importing node module fs in a constant fs
+const fs = require("fs");
+
+// Create a file notes.txt and will write the text to the same
+fs.writeFileSync("notes.txt", "This file is created from NodeJs.");
+// This will append the data to file
+fs.appendFileSync("notes.txt", "This is Saurabh Jain");
